3a4d57bb239af4b9d12d037452f0a5a5be9590a1
josephtoles <jbtoles@gmail.com>
2016-11-07 01:00:25 -0800

    Added base keys

---
+++ practice.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+keys = ('abcdefghijklmnopqrstuvwxyz'
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        '`1234567890-='
+        '~!@#$%^&*()_+'
+        '[]\\'
+        '{}|'
+        ';\''
+        ':"'
+        ',./'
+        '<>?')
+
+print keys
