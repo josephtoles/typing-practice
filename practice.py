7cbdea6aaa94b5a08cb81b93a95fdd930af6aa4d
josephtoles <jbtoles@gmail.com>
2016-11-07 01:26:54 -0800

    Practice script works

--- practice.py
+++ practice.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
+import random
+import sys
+import datetime
 
 keys = ('abcdefghijklmnopqrstuvwxyz'
-        'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        #'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
         '`1234567890-='
         '~!@#$%^&*()_+'
         '[]\\'
@@ -9,6 +12,37 @@ keys = ('abcdefghijklmnopqrstuvwxyz'
         ';\''
         ':"'
         ',./'
-        '<>?')
+        '<>?'
+        ' ')
 
-print keys
+keystrokes_per_try = 12
+
+def get_random_character():
+    return random.choice(keys)
+
+def get_random_characters(keystrokes_per_try=keystrokes_per_try):
+    output = ''
+    for i in range(0, keystrokes_per_try):
+        output += get_random_character()
+    return output
+
+def test_player(test_letters):
+    start_time = datetime.datetime.now()
+    player_letters = raw_input(test_letters + '\n')
+    if (player_letters == test_letters):
+        end_time = datetime.datetime.now()
+        delta = end_time - start_time
+        if (delta > datetime.timedelta(minutes=1)):
+            print 'correct, but you took too long'
+        else:
+            seconds = delta.seconds
+            micros = delta.microseconds
+            print 'good job with time ' + str(seconds) + '.' + str(micros)
+    else:
+        print 'mistake. try again'
+        test_player(test_letters)
+
+while (True):
+    test_letters = get_random_characters()
+    test_player(test_letters)
+
